6990878557023580e0820c934e148af809e81300
Aditya R <89331580+adi-42@users.noreply.github.com>
2023-01-29 12:22:34 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+cd "$(dirname "${BASH_SOURCE[0]}")"
+BASE=$(pwd)
+
+git config --global core.editor vim
+
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+
+ln -sTfv $BASE/vim ~/.vim
+ln -sfv $BASE/my_bashrc.sh ~/.oh-my-bash/custom/my_bashrc.sh
+
+#if it is wsl
+WSL=$(uname -a | grep 'WSL')
+if [ -n "$WSL" ]; then
+  sudo bash -c "echo -e '[network]\ngenerateResolvConf = false' > /etc/wsl.conf ; echo 'nameserver 1.1.1.1' > /etc/resolv.conf"
+fi
+
+#if it is debian
+if [  -f /etc/debian_version ]  ; then
+  sudo apt install fzf -y
+  sudo apt install vim -y
+  sudo apt update && sudo apt upgrade -y && sudo apt full-upgrade -y && sudo apt autoremove -y
+  sudo apt install locales-all
+fi
+
+#if it is mac
+if [ "$(uname)" == "Darwin" ] ; then
+  sudo 
+
+fi
+
+vim +PlugInstall +qall
